81d5420462aab21145dc304c9a82b18055d32b66
vigneshkar03 <vigneshkar03@gmail.com>
2023-07-30 12:35:45 +0530

    Fisrt commit

---
+++ src/test/java/a1_googleTest/A1_GoogleTest.java
@@ -0,0 +1,38 @@
+package a1_googleTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class A1_GoogleTest {
+
+
+	@Test
+	public void validateGoogleTest() throws IOException{
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver wd=new ChromeDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.google.com/");
+		System.out.println(wd.getTitle());
+		wd.findElement(By.name("q")).sendKeys("chennai");
+		wd.findElement(By.name("q")).submit();
+		System.out.println(wd.getTitle());
+		
+		File src= ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(src, new File("C:/Batch261/J1_GitTest/screenshot/google.png"));
+		
+		wd.quit();
+	}
+}
+
+
